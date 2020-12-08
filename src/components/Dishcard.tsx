2758cd27e305b63c151a6812768b661250714b36
Jimmy Lam <w.h.lam@student.tudelft.nl>
2020-12-08 21:56:43 +0100

    [WIP] First version of the dishcard

--- src/components/Dishcard.tsx
+++ src/components/Dishcard.tsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import Card from 'react-bootstrap/Card';
+import React from "react";
+import "../css/Dishcard.css";
 
-
-function Dishcard(){
-    return(
-        <Card style={{ width: '18rem' }}>
-            <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-                <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
-                </Card.Text>
-                <Card.Link href="#">Card Link</Card.Link>
-                <Card.Link href="#">Another Link</Card.Link>
-            </Card.Body>
-        </Card>
-    );
+function Dishcard(props: DProps) {
+  return (
+    <button className={`dishcard`}>
+      <p>{props.details.title}</p>
+      <p>{props.details.price}</p>
+    </button>
+  );
 }
 
 export default Dishcard;
+
+interface DProps {
+  details: {
+    title: string;
+    price: number;
+    image?: string;
+  };
+  children?: any;
+}
