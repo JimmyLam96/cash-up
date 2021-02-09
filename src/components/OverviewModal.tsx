4d2a8b9a43c85865d09735327f4aa8515b8cc00a
Jimmy Lam <w.h.lam@student.tudelft.nl>
2021-02-09 10:33:18 +0100

    [WIP] Redesigned delivery modal

--- src/components/OverviewModal.tsx
+++ src/components/OverviewModal.tsx
@@ -1,23 +1,71 @@
 import React from "react";
 import "../css/OverviewModal.css";
-import * as HiIcons from "react-icons/hi";
-import * as BsIcons from "react-icons/bs";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
 
 function OverviewModal() {
+  const [isActive, setIsActive] = useState(false);
+  const [value, setValue] = useState("");
+  const { register, handleSubmit, errors } = useForm();
+
+  function handleTextChange(text: string) {
+    setValue(text);
+
+    if (text !== "") {
+      setIsActive(true);
+    } else {
+      setIsActive(false);
+    }
+  }
+
   return (
     <div className="overview-modal">
-      <form>
-        <HiIcons.HiLocationMarker color="#00a99d" />
-        <input type="text" name="name" />
-      </form>
-      <form>
-        <HiIcons.HiClock color="#00a99d" />
-        <input type="text" name="name" />
-      </form>
-      <form>
-        <BsIcons.BsFillPersonFill color="#00a99d" />
-        <input type="text" name="name" />
-      </form>
+      <div className="section">
+        <div className="float-label">
+          <input
+            type="address"
+            value={value}
+            onChange={(e) => handleTextChange(e.target.value)}
+          />
+          <label className={isActive ? "Active" : ""} htmlFor="address">
+            address
+          </label>
+        </div>
+        <div className="float-label number-label">
+          <input
+            min={1}
+            type="number"
+            value={value}
+            onChange={(e) => handleTextChange(e.target.value)}
+          />
+          <label className={isActive ? "Active" : ""} htmlFor="housenumber">
+            number
+          </label>
+        </div>
+      </div>
+      <div className="section">
+        <div className="float-label number-label">
+          <input
+            min={1}
+            type="postalcode"
+            value={value}
+            onChange={(e) => handleTextChange(e.target.value)}
+          />
+          <label className={isActive ? "Active" : ""} htmlFor="postalcode">
+            postalcode
+          </label>
+        </div>
+        <div className="float-label">
+          <input
+            type="address"
+            value={value}
+            onChange={(e) => handleTextChange(e.target.value)}
+          />
+          <label className={isActive ? "Active" : ""} htmlFor="email">
+            city
+          </label>
+        </div>
+      </div>
     </div>
   );
 }
