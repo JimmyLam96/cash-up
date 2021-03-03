b119662df284ca3f32c6db4f25a2a9c310fc0970
Jimmy Lam <w.h.lam@student.tudelft.nl>
2021-03-03 16:14:53 +0100

    [WIP] Switched over to NestJS with complete implementation

--- backend/src/index.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import {Request, Response} from "express-serve-static-core"
- 
-const app = express();
-const port = 3000;
-
-app.use('/dish', require('./routes/addDish'));
- 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
